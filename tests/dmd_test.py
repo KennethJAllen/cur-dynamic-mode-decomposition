83a59eb851a03cc32772ff64a2e49439386e8a4a
Kenneth <kencubed@gmail.com>
2023-11-25 14:22:59 -0500

    updated example fixture and corresponding test

--- tests/dmd_test.py
+++ tests/dmd_test.py
@@ -8,12 +8,12 @@ from dynamic_mode_decomposition import dmd
 @pytest.fixture
 def example_upper_triangular_matrix():
     '''An example matrix'''
-    example_upper_triangular_matrix = np.array([[1, 2], [0, 4]])
+    example_upper_triangular_matrix = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
     return example_upper_triangular_matrix
 
 # test dmd
-def test_eigenvalues_of_dmd(example_upper_triangular_matrix):
+def test(example_upper_triangular_matrix):
     '''Test the eigenvalues of the dynamic mode decomposition.'''
     rank = example_upper_triangular_matrix.shape[0]
     [eig_values, _] = dmd.dmd(example_upper_triangular_matrix, rank)
-    assert np.array_equal(eig_values, np.diag(example_upper_triangular_matrix))
+    assert pytest.approx(eig_values[0], 0)

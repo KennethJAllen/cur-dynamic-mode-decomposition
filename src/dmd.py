99511db308ae76e568947aa4a61f74d4e3e2a473
Kenneth <kencubed@gmail.com>
2025-01-04 17:04:43 -0500

    implemented cur dmd

--- src/dmd.py
+++ src/dmd.py
@@ -25,26 +25,61 @@ def cur_dmd(data: np.ndarray,
     Example:
         dmd_eig_values, dmd_modes = dmd(np.random.rand(100, 10), 5)
     """
-    if initial_row_indices is None:
-        n, _ = data.shape
-        initial_row_indices = np.random.randint(n, size=rank)
-    if initial_column_indices is None:
-        _, p = data.shape
-        initial_column_indices = np.random.randint(p, size=rank)
-    if len(initial_row_indices) != rank or len(initial_column_indices) != rank:
-        raise ValueError("The number of initial indices must be equal to the desired rank.")
-    optimal_row_indices, optimal_column_indices = mc.alternating_maxvol(data, initial_row_indices, initial_column_indices)
-    c, u, r = mc.cur_decomposition(data, optimal_row_indices, optimal_column_indices)
-
+    # data matrices
+    x = data[:,:-1] # All but last column
     y = data[:,1:] # All but first column
+
+    # If the initial indices are not valid or do not exist, get valid ones
+    row_indices, column_indices = get_valid_initial_indices(x, rank, initial_row_indices, initial_column_indices)
+    optimal_row_indices, optimal_column_indices = mc.alternating_maxvol(x, row_indices, column_indices)
+    c, u, r = mc.cur_decomposition(x, optimal_row_indices, optimal_column_indices)
 
     # Analogous to Koopman operator
     pinv_r = LA.pinv(r)
     a_tilde = LA.pinv(c) @ y @ pinv_r @ u
     dmd_eig_values, w = LA.eig(a_tilde)
-    dmd_modes = y @ pinv_r * u @ w
+    # Multiply in reverse order for efficiency
+    dmd_modes = y @ (pinv_r @ (u @ w))
 
     return dmd_eig_values, dmd_modes
+
+def get_valid_initial_indices(data: np.ndarray,
+                              desired_rank: int,
+                              initial_row_indices: np.ndarray = None,
+                              initial_column_indices: np.ndarray = None,
+                              max_attempts: int = 1000) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Finds valid valid row and column indices for use in the alternating maxvol algorithm.
+    If initial_row_indices and initial_column_indices are provided, checks if they correspond to a valid submatrix first.
+    Initial indices are considered valid if they correspond to a full rank submatrix in the data.
+    The algorithm resamples indicies until a full rank submatrix is found.
+    A more efficint implementation is possible, one idea is to first resample indices that correspond to rows/columns of all zeros.
+    """
+    valid_indices = False
+    if initial_row_indices is not None and initial_column_indices is not None:
+        try:
+            if len(initial_row_indices) != len(initial_column_indices): # checks that initial submatrix is square
+                raise ValueError("Initial submatrix is not square.")
+            initial_submatrix = data[initial_row_indices,:][:,initial_column_indices]
+            initial_submatrix_rank = np.linalg.matrix_rank(initial_submatrix)
+            if initial_submatrix_rank==desired_rank:
+                valid_indices = True
+        except ValueError:
+            print("Initial indicies are not valid, finding new indices...")
+
+    n, p = data.shape
+    num_attemps = 0
+    while not valid_indices:
+        if num_attemps > max_attempts:
+            raise ValueError(f"Could not find a valid initial submatrix in {max_attempts} attempts.")
+        initial_row_indices = np.random.randint(n, size=desired_rank)
+        initial_column_indices = np.random.randint(p, size=desired_rank)
+        submatrix = data[initial_row_indices,:][:,initial_column_indices]
+        submatrix_rank = np.linalg.matrix_rank(submatrix)
+        if submatrix_rank==desired_rank: # Rank is valid when submatrix is full rank.
+            valid_indices = True
+        num_attemps += 1
+    return initial_row_indices, initial_column_indices
 
 def svd_dmd(data: np.ndarray, rank: int) -> tuple[np.ndarray, np.ndarray]:
     """
@@ -98,7 +133,7 @@ def forecast(data: np.ndarray, rank: int, num_forecasts: int, cur_or_svd: str = 
     forecast_results = np.zeros((data_dimension, num_forecasts), dtype=np.complex_)
 
     # time stepping
-    low_dim_forecast = LA.lstsq(dmd_modes, initial_condition)[0]
+    low_dim_forecast = LA.lstsq(dmd_modes, initial_condition, rcond=None)[0]
     for t in range(num_forecasts):
         low_dim_forecast = diag_eig_values @ low_dim_forecast
         forecast_results[:,t] =  dmd_modes @ low_dim_forecast
--- src/maxvol_cur.py
+++ src/maxvol_cur.py
@@ -4,56 +4,44 @@ from numpy import linalg as LA
 
 def cur_decomposition(data: np.ndarray,
                       row_indices: np.ndarray,
-                      column_indices: np.ndarray,
-                      validate_rank: bool = False) -> tuple[np.ndarray]:
+                      column_indices: np.ndarray) -> tuple[np.ndarray]:
     """
     Returns the CUR decomposition of data with respect to the row and column indices.
     Optimal row and column indices are given by the maximum volume algorithm
     data is approximated by C @ inv(U) @ R, with equality when the rank of the data is equal to the number of indices.
     """
-    num_rows = len(row_indices)
-    if num_rows != len(column_indices):
+    if len(row_indices) != len(column_indices):
         raise ValueError("Number or row and column indices must be equal.")
     columns = data[:, column_indices] # C
     submatrix = data[row_indices, :][:, column_indices] # U
     rows = data[row_indices, :] # R
-    if validate_rank:
-        submatrix_rank = np.linalg.matrix_rank(submatrix)
-        if submatrix_rank != num_rows:
-            print(f"Warning: Submatrix in row, column indices is not invertible. Rank {submatrix_rank} when expecting rank {num_rows}.")
     return columns, submatrix, rows
 
 def alternating_maxvol(data: np.ndarray,
-                       initial_row_indices: np.ndarray,
-                       initial_column_indices: np.ndarray,
-                       validate_rank: bool = False) -> tuple[np.ndarray, np.ndarray]:
+                       initial_row_indices: np.ndarray = None,
+                       initial_column_indices: np.ndarray = None) -> tuple[np.ndarray, np.ndarray]:
     """
     Algorithm based on paper "How to Find a Good Submatrix" by Goreinov, Sergei A., et al.
     Alternating version of the one directional maxvol algorithm, modified for two-directional search.
-    Finds a close to dominant r x r submatrix of data matrix.
+    Finds a close to dominant square submatrix of data matrix.
 
     The submatrix in the resulting indices is the corresponding close to maximum volume submatrix.
     In other words, if A = data[row_indices,:][:,column_indices],
     then abs(det(A)): is close to maximum over all choices of submatrixes.
     """
+    num_indices = len(initial_row_indices)
+    num_column_indices = len(initial_column_indices)
+    if num_indices != len(initial_column_indices): # checks that initial submatrix is square
+        raise ValueError(f"initial submatrix is not square. Num rows: {num_indices}. Num columns: {num_column_indices}.")
+
+    submatrix = data[initial_row_indices,:][:,initial_column_indices] # initial submatrix
     row_indices = initial_row_indices.copy()
     column_indices = initial_column_indices.copy()
     error_threshold = 1e-8 # tolerance
-
-    num_indices = len(row_indices)
-    if num_indices != len(column_indices): # checks that initial submatrix is r x r
-        raise ValueError("initial submatrix is not r x r")
-
-    submatrix = data[row_indices,:][:,column_indices] # initial submatrix
-    if validate_rank:
-        initial_rank = np.linalg.matrix_rank(submatrix)
-        if initial_rank != num_indices: # initial submatrix should be nonsingular
-            print(f"Warning: Submatrix in positions I and J is not invertible. Rank {initial_rank} when expecting rank {num_indices}.")
-
     row_dom = False # indicates if near dominant in rows
     column_dom = False # indicates if near dominant in columns
+    max_num_iterations = 1000
 
-    max_num_iterations = 1000 #
     for k in range(max_num_iterations):
         Yh = LA.solve(submatrix.T, data[:,column_indices].T)
         Y = Yh.T # Y =  data[:,J]A^{-1}
@@ -71,7 +59,7 @@ def alternating_maxvol(data: np.ndarray,
         else:
             column_dom = True # indicates that A is near dominant in columns
 
-        Z = LA.solve(submatrix,data[row_indices,:]) #Z = A^{-1}data[I,:]
+        Z = LA.solve(submatrix, data[row_indices,:]) #Z = A^{-1}data[I,:]
         Za = np.abs(Z) # entry-wise absolute value of Z
         z = np.amax(Za) # largest element of Z in modulus
         if z > 1+error_threshold: # if A is not within the acceptable tolerance of a dominant submatrix in rows

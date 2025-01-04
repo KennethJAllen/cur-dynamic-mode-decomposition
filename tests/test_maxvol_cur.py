0a1c9e2f16e0a97823d68c9aa8dd9be66393b463
Kenneth <kencubed@gmail.com>
2025-01-04 17:38:08 -0500

    added test for cur decomposition

--- tests/test_maxvol_cur.py
+++ tests/test_maxvol_cur.py
@@ -13,5 +13,13 @@ def test_alternating_maxvol(upper_triangular_matrix):
     initial_row_indices = np.array([0])
     initial_column_indices = initial_row_indices
     row_indices, column_indices = mc.alternating_maxvol(upper_triangular_matrix, initial_row_indices, initial_column_indices)
-    assert row_indices[0] == 1
-    assert column_indices[0] == 2
+    assert (row_indices == np.array([1])).all()
+    assert (column_indices == np.array([2])).all()
+
+def test_cur_decomposition(upper_triangular_matrix):
+    row_indices = np.array([0, 1])
+    column_indices = np.array([1, 2])
+    c, u, r = mc.cur_decomposition(upper_triangular_matrix, row_indices, column_indices)
+    assert (c == np.array([[2, 3],[1, 4],[0, 1]])).all()
+    assert (u == np.array([[2, 3],[1, 4]])).all()
+    assert (r == np.array([[1, 2, 3],[0, 1, 4]])).all()
